ef33981e50b16d8f7080c35aedf1c73061d5350b
Pasindu Subasinghe <phimansa@gmail.com>
2026-02-06 02:20:38 +0530

    bind method

--- Functions/objectMethods.js
+++ Functions/objectMethods.js
@@ -1,10 +1,25 @@
-var student = {
+function displayDetails(testscore, testscore2) {
+  document.write(this.id + "<br/>");
+  document.write(this.name + "<br/>");
+  document.write(testscore + "<br/>");
+  document.write(testscore2 + "<br/>");
+}
+
+var student1 = {
   id: 1,
   name: "Bob",
-  display: function () {
-    document.write(student.id + "<br/>");
-    document.write(student.name + "<br/>");
-  },
 };
 
-student.display();
+var student2 = {
+  id: 2,
+  name: "John",
+};
+
+this.id = 3;
+this.name = "Mary";
+
+var displayForStudent1 = displayDetails.bind(student1, 10, 30);
+displayForStudent1();
+
+var displayForStudent2 = displayDetails.bind(student2, 20, 40);
+displayForStudent2();

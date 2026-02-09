ad6c54036a38c35ce0bdec35d636da1212b9c4a3
Pasindu Subasinghe <phimansa@gmail.com>
2026-02-09 16:14:37 +0530

    Seal objects

---
+++ Objects/preventExtensions.js
@@ -0,0 +1,13 @@
+var product = {
+  name: "Iphone",
+};
+
+console.log(Object.isExtensible(product));
+
+Object.preventExtensions(product);
+
+console.log(Object.isExtensible(product));
+
+product.price = 700;
+
+console.log("price" in product);

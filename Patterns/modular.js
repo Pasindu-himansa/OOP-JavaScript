486b467a9d049f11056f0403fd8ca53b8d3739be
Pasindu Subasinghe <phimansa@gmail.com>
2026-03-01 20:19:23 +0530

    revealling modular patterns

---
+++ Patterns/modular.js
@@ -0,0 +1,25 @@
+var account = (function () {
+  var balance = 1000;
+  function getBalance() {
+    return balance;
+  }
+  function addIntrest(interest) {
+    balance += interest;
+  }
+
+  return {
+    name: "John",
+    getBalance: getBalance,
+    addIntrest: addIntrest,
+  };
+})();
+
+console.log(account.name);
+console.log(account.getBalance());
+
+account.balance = 2000;
+
+console.log(account.getBalance());
+
+account.addIntrest(300);
+console.log(account.getBalance());

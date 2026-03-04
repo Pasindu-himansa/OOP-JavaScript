f39b402595bd0a032af934fed6ad2eb14d729597
Pasindu Subasinghe <phimansa@gmail.com>
2026-03-04 19:44:04 +0530

    Array destructuring

---
+++ Destructuring/array.js
@@ -0,0 +1,7 @@
+const courses = ["Angular", "React", "Node"];
+
+const [course1, course2, course3] = courses;
+
+console.log(course1); // Angular
+console.log(course2); // React
+console.log(course3); // Node

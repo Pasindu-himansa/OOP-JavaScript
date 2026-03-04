47806cfd00f6d3cb37518cdee638abad3fccf02e
Pasindu Subasinghe <phimansa@gmail.com>
2026-03-04 19:37:08 +0530

    Object destructuring

---
+++ Destructuring/object.js
@@ -0,0 +1,11 @@
+let student = {
+  firstName: "John",
+  lastName: "Bailey",
+  score: 90,
+};
+
+const { firstName, lastName, score } = student;
+
+console.log(firstName); // John
+console.log(lastName); // Bailey
+console.log(score); // 90

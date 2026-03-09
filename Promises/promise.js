8be21bc08c49f3e5ebcc19644525c73a6b6cb0df
Pasindu Subasinghe <phimansa@gmail.com>
2026-03-09 23:48:08 +0530

    Catch error

--- Promises/promise.js
+++ Promises/promise.js
@@ -1,10 +1,19 @@
 function myAsyncFunction() {
   let promise = new Promise((resolve, reject) => {
+    let error = true;
     setTimeout(() => {
       console.log("working asyncronously");
     }, 2000);
+    if (error) {
+      reject("Error");
+    } else {
+      resolve("Done");
+    }
   });
   return promise;
 }
 
-myAsyncFunction().then(() => console.log("Work done"));
+myAsyncFunction().then(
+  (success) => console.log(success),
+  (error) => console.log(error),
+);
